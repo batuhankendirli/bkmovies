493e5cd9a560ae49758a1e4a222fe4a48af6b61b
batuhan kendirli <batuhankndrl@gmail.com>
2022-09-05 23:42:15 +0300

    sidebar and its data removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
 import SectionMain from './components/SectionMain';
 import UserPanel from './components/UserPanel';
 
 function App() {
   return (
     <div className="App">
-      {/* <Sidebar /> */}
       <SectionMain />
       <UserPanel />
     </div>
