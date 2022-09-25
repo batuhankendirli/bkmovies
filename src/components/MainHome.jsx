7fc2f21dc78c2806f26708af2fb7f2063f74d8fa
batuhan kendirli <batuhankndrl@gmail.com>
2022-09-25 19:19:51 +0300

    home page created

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function MainHome() {
   return (
-    <div className="home">
-      <h1 className="home-header">Welcome to BKMovies</h1>
-    </div>
+    <>
+      <div className="home">
+        <img src="/img/welcome.png" alt="Scene from Fight Club" className="home-img" />
+        <div className="home-wrapper">
+          <div className="home-wrapper-texts">
+            <h1 className="home-wrapper-texts-header">Welcome to BKMovies!</h1>
+            <p className="home-wrapper-texts-paragraph">
+              BKMovies lets you keep track of every movie and tv show you want
+              to see. And you can watch the trailers of your favourite movies
+              and tv shows.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
